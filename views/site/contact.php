65c01c353301ee13433ff6e3eef2d0d891521d6d
Alice <a@mail.ru>
2022-12-01 13:38:47 +0300

    pages and page content

--- views/site/contact.php
+++ views/site/contact.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     <div class="row mt-2">
-        <div class="col-lg-12">
+        <div class="col-lg-12 contacts-map">
             <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A79e6e8a0c31ed764a2933255e031d39f2bc1f9b757465dcf7e7e5b42b098ae8c&amp;width=1200&amp;height=600&amp;lang=ru_RU&amp;scroll=false"></script>
         </div>
     </div>

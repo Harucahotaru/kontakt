99e6720dce8bc4ce7152c041094ea597f8c6896b
Alice <agribova@team.amocrm.com>
2023-01-11 11:44:39 +0300

    1) исправлен баг с неправильными переходами для pjax

--- views/catalog/view.php
+++ views/catalog/view.php
@@ -4,10 +4,8 @@ use app\controllers\CatalogController;
 use app\models\Products;
 use app\models\Reviews;
 use app\widgets\ParentProducts;
-use kartik\editors\Summernote;
+use app\widgets\ViewedProducts;
 use kartik\rating\StarRating;
-use yii\bootstrap5\ActiveForm;
-use yii\bootstrap5\Html;
 
 /** @var  Products $model */
 
@@ -16,6 +14,7 @@ $this->params['breadcrumbs'][] = ['label' => 'Каталог', 'url' => ['index'
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 CatalogController::setViewedProductsCookie($model->id);
+
 ?>
 <div class="container catalog-view-container">
     <div>
@@ -149,19 +148,25 @@ CatalogController::setViewedProductsCookie($model->id);
         </div>
     </div>
 </div>
+
+<!-- модуль подходящих товаров -->
 <div class="container-grey <?= (!is_array($model->parent_id)) ? 'catalog-view-display-none' : '' ?>">
     <div class="container py-4">
         <div class="catalog-title-size py-2">Вместе с этим вы можете приобрести</div>
         <?= (is_array($model->parent_id)) ? ParentProducts::widget(['parentIds' => $model->parent_id]) : '' ?>
     </div>
 </div>
+
+<!-- модуль ранее просмотренных товаров -->
 <?php $cookieProducts = CatalogController::getViewedProductsIds($model->id); ?>
 <div class="container-grey <?= (empty($cookieProducts)) ? 'catalog-view-display-none' : '' ?>">
     <div class="container py-4">
         <div class="catalog-title-size py-2">Вы недавно смотрели</div>
-        <?= !empty($cookieProducts) ? ParentProducts::widget(['parentIds' => $cookieProducts]) : '' ?>
+        <?= !empty($cookieProducts) ? ViewedProducts::widget(['parentIds' => $cookieProducts]) : '' ?>
     </div>
 </div>
+
+<!-- модуль отзывов -->
 <div class="container-grey">
     <a name="reviews"></a>
     <div class="container py-4">
@@ -173,92 +178,7 @@ CatalogController::setViewedProductsCookie($model->id);
                 Оставить свой отзыв
             </button>
             <!-- Modal -->
-            <?php $form = ActiveForm::begin(['action' => '/products-reviews/create']); ?>
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                 aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Отзыв на товар: <?= $model->name ?></h5>
-                        </div>
-                        <div class="modal-body">
-                            <div>
-                                <?php $reviewModel = new Reviews(); ?>
-                                <div>
-                                    Общая оценка:
-                                    <?= $form->field($reviewModel, 'rate')->widget(StarRating::class, [
-                                        'id' => 'test',
-                                        'language' => 'ru',
-                                        'value' => 3,
-                                        'pluginOptions' => [
-                                            'showCaption' => false,
-                                            'clearButton' => '',
-                                            'step' => 1,
-                                        ]
-                                    ])->label(false); ?>
-                                </div>
-                                <div class="py-1">Срок использования:</div>
-                                <?= $form->field($reviewModel, 'experience')->dropDownList(Products::UseTermList(), [
-                                        'prompt' => 'Сколько вы использовали товар?',
-                                    ]
-                                )->label(false) ?>
-                                <div class="py-1">Преимущества:</div>
-                                    <?= $form->field($reviewModel, 'benefits')->widget(Summernote::class, [
-                                        'useKrajeeStyle' => true,
-                                        'useKrajeePresets' => true,
-                                        'language' => 'ru',
-                                        'enableFullScreen' => false,
-                                        'pluginOptions' => [
-                                            'height' => 100,
-                                            'allowClear' => true,
-                                            'toolbarOptions' => false,
-                                            'toolbar' => false,
-                                        ],
-                                    ]
-                                )->label(false); ?>
-                                <div class="py-1">Недостатки:</div>
-                                <?= $form->field($reviewModel, 'limitations')->widget(Summernote::class, [
-                                        'useKrajeeStyle' => true,
-                                        'useKrajeePresets' => true,
-                                        'language' => 'ru',
-                                        'enableFullScreen' => false,
-                                        'pluginOptions' => [
-                                            'height' => 100,
-                                            'allowClear' => true,
-                                            'toolbarOptions' => false,
-                                            'toolbar' => false,
-                                        ],
-                                    ]
-                                )->label(false); ?>
-                                <div class="py-1">Опыт использования:</div>
-                                <?= $form->field($reviewModel, 'content')->widget(Summernote::class, [
-                                        'useKrajeeStyle' => true,
-                                        'useKrajeePresets' => true,
-                                        'language' => 'ru',
-                                        'enableFullScreen' => false,
-                                        'pluginOptions' => [
-                                            'height' => 100,
-                                            'allowClear' => true,
-                                            'toolbarOptions' => false,
-                                            'toolbar' => false,
-                                        ],
-                                    ]
-                                )->label(false); ?>
-                                <?= $form->field($reviewModel, 'user_id')->hiddenInput(['value' => Yii::$app->user->id])->label(false); ?>
-                                <?= $form->field($reviewModel, 'entity_id')->hiddenInput(['value' => $model->id])->label(false); ?>
-                            </div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-                            <div class="form-group">
-                                <?= Html::submitButton('Сохранить отзыв', ['class' => 'btn btn-warning']) ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php ActiveForm::end(); ?>
+            <?= $this->render("@app/views/catalog/productReviewModal", ['model' => $model]) ?>
         </div>
         <div>
             <?= \app\widgets\Reviews::widget(['type' => Reviews::REVIEW_TYPE_PRODUCTS, 'entityId' => $model->id]) ?>

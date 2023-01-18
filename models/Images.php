a330fbea8a051366f4a3295e33268bf383c2d142
Alice <agribova@team.amocrm.com>
2023-01-18 12:33:44 +0300

    code

--- models/Images.php
+++ models/Images.php
@@ -124,7 +124,7 @@ class Images extends \yii\db\ActiveRecord
     {
 
         $thumbnailsPath = $this->getPreviewPath();
-        var_dump($this->appFullPath);exit();
+//        var_dump($this->appFullPath);exit();
         $image = new ImageResize('https://kontakt-arm.ru/upload/images/products/1.jpg');
         $image->resize(400, 400, true);
         $image->save(Yii::getAlias('@webroot') . $thumbnailsPath, IMAGETYPE_PNG);

5ac42ea06c198ded797c967099b1be5f10ce276d
Alice <agribova@team.amocrm.com>
2023-01-26 13:23:33 +0300

    добавлена заглушка фото для карточки товара

--- widgets/views/parent-products/_products_item.php
+++ widgets/views/parent-products/_products_item.php
@@ -7,16 +7,18 @@ use app\models\Products;
 ?>
 <div class="product-card my-3">
     <a data-pjax=0 href="/catalog/view/<?= $model->id ?>">
-        <section class="swiper-container swiper1 pt-3">
-            <div class="swiper-wrapper">
-                <?php foreach ($model->getThumbnailsPath() as $image): ?>
-                    <div class="swiper-slide" style="height: 250px">
-                        <img style="width: 100%" src="<?= $image ?>" alt=""/>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-            <div class="swiper-pagination swiper1 swiper-pagination-white"></div>
-        </section>
+        <div class="<?= empty($model->getThumbnailsPath()) ? 'no-image-product' : '' ?>">
+            <section class="swiper-container swiper1 pt-3">
+                <div class="swiper-wrapper">
+                    <?php foreach ($model->getThumbnailsPath() as $image): ?>
+                        <div class="swiper-slide" style="height: 250px">
+                            <img style="width: 100%" src="<?= $image ?>" alt=""/>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <div class="swiper-pagination swiper1 swiper-pagination-white"></div>
+            </section>
+        </div>
         <div class="product-card-body">
             <h5 class="product-card-title"><?= $model->name ?></h5>
             <div class="product-card-description"><?= $model->description ?></div>
--- widgets/views/viewed-products/_products_item.php
+++ widgets/views/viewed-products/_products_item.php
@@ -3,16 +3,18 @@
 ?>
 <div class="product-card my-3">
     <a data-pjax=0 href="/catalog/view/<?= $model->id ?>">
-        <section class="swiper-container swiper2 pt-3">
-            <div class="swiper-wrapper">
-                <?php foreach ($model->getThumbnailsPath() as $image): ?>
-                    <div class="swiper-slide" style="height: 250px">
-                        <img style="width: 100%" src="<?= $image ?>" alt=""/>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-            <div class="swiper-pagination swiper2 swiper-pagination-white"></div>
-        </section>
+        <div class="<?= empty($model->getThumbnailsPath()) ? 'no-image-product' : '' ?>">
+            <section class="swiper-container swiper2 pt-3">
+                <div class="swiper-wrapper">
+                    <?php foreach ($model->getThumbnailsPath() as $image): ?>
+                        <div class="swiper-slide" style="height: 250px">
+                            <img style="width: 100%" src="<?= $image ?>" alt=""/>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <div class="swiper-pagination swiper2 swiper-pagination-white"></div>
+            </section>
+        </div>
         <div class="product-card-body">
             <h5 class="product-card-title"><?= $model->name ?></h5>
             <div class="product-card-description"><?= $model->description ?></div>
